c3d50778e19c695df75527cb41135761215dfebd
Lian <Lian.Duan@alumni.ubc.ca>
2019-02-24 16:49:19 -0800

    Update stripeColorPicker whiteyboi
    Whitebois

--- stripeColorPicker.cpp
+++ stripeColorPicker.cpp
@@ -2,10 +2,28 @@
 
 stripeColorPicker::stripeColorPicker(HSLAPixel fillColor, int stripeSpacing)
 {
-/* your code here! */
+    stripeColorPicker->color = fillColor;
+    stripeColorPicker->spacing = stripeSpacing;
 }
 
 HSLAPixel stripeColorPicker::operator()(int x, int y)
 {
-/* your code here! */
+    // If the x or y coordinate is a
+    //  * multiple of the spacing, it will be filled with the fillColor.
+    //  * otherwise, it will be filled with white.
+
+    //This means modulo of the colors stuff with the spacing given;
+
+    if (x % stripeColorPicker->spacing || y % stripeColorPicker->spacing){
+        //This gives us the set stripe color
+        return stripeColorPicker->color;
+    }
+    else {
+        //Otherwise it's a whitey boi
+        HSLAPixel whiteboi;
+        whiteboi.h = 0;
+        whiteboi.s = 0;
+        whiteboi.l = 100;
+        return whiteboi;
+    }
 }
