a3620c2c1842fa455ca759c17e2e7146172f2bb8
LianD <Lian.Duan@Alumni.ubc.ca>
2019-03-01 13:29:05 -0800

    Generic Custom Picker

---
+++ customColorPicker.h
@@ -0,0 +1,21 @@
+#ifndef CUSTOMCOLORPICKER_H
+#define CUSTOMCOLORPICKER_H
+
+#include "colorPicker.h"
+
+/**
+ * stripeColorPicker: a functor that determines the color that should be used
+ * given an x and a y coordinate using a stripe pattern.
+ *
+ */
+class CustomColorPicker : public colorPicker
+{
+  public:
+    CustomColorPicker(HSLAPixel fillColor, int stripeSpacing);
+    
+    virtual HSLAPixel operator()(int x, int y);
+
+  private:
+    HSLAPixel color; /**< Color used for the stripe. */
+    int spacing;     /**< Space between stripes. */
+};
